d1052ee9fef605350714b1b3c0508187fb3ba9df
Thquan105 <thquan105@gmail.com>
2024-12-28 22:49:19 +0700

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,8 @@
 
 python manage.py makemigrations
 python manage.py migrate
+
+python manage.py createsuperuser --noinput
+
+python manage.py collectstatic
 python manage.py runserver 0.0.0.0:8000 
--- thq_core/settings.py
+++ thq_core/settings.py
@@ -5,7 +5,7 @@ from pathlib import Path
 load_dotenv()
 
 ENVIRONMENT = os.getenv("ENVIRONMENT", "production")
-ENVIRONMENT = 'production'
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
